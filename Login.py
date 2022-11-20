2e871ed18a973e518a3a04fb064b7d4f75744691
GustavoAquino36 <gustavoaquino357@gmail.com>
2022-11-20 19:29:57 -0300

    Fix/name-and-organization

--- Login.py
+++ Login.py
@@ -5,41 +5,6 @@ import os
 def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-def csvfile():
-    userscsv = open('logInfo.csv', 'r')
-    linhas = csv.reader(userscsv, delimiter = ',')
-    dataarray = []
-    for log in linhas:
-        dataarray.append(log)
-    for i in dataarray:
-        print(i)
-    
-def userinfo():
-    global user, dataobj
-    clear()
-    user = input('Qual user Quer puxar as informacoes? (email e linkedin)\n').casefold()
-    ruser = open('userInfo.csv', 'r')
-    linhas = csv.reader(ruser, delimiter=',')
-    dataobj = {}
-    for log in linhas:
-        dataobj[log[0]] = log[1:]
-    showUserInfo()
-def showUserInfo():
-    try:
-        clear()
-        info = print(f'User: {user} \nEmail: {dataobj[user][0]} \nLinkedin: {dataobj[user][1]}')
-        backToMenu = input(f'[0] Para retornar ao menu inicial\n ')
-        while not backToMenu == '0':
-            showUserInfo()
-        else:
-            mainMenu()
-    except KeyError:
-        noUserError = input(f'User {user} nao encontrado, Gostaria de Registra-lo?\n')
-        if noUserError.casefold() in ['sim','yes','y','s','quero']:
-            registra()
-        else:
-            mainMenu()
-
 def mainMenu():
     clear()
     global escolha
@@ -55,18 +20,56 @@ def mainMenu():
 
 def cursor():
     if escolha == '1':
-        return registra()
+        return register()
     elif escolha == '2':
         return userinfo()
+    elif escolha == '0':
+        print('Fechando crud...')
 
-def registra():
+def register():
     clear()
     try:
         user = input('User a ser registrado: ')
         senha = getpass('Senha a ser registrada: ')
-        data = open('txt data base path here', 'a')
+        data = open('logInfo.csv', 'a')
         data.write(f'\n{user},{senha}')
     finally:
         return mainMenu()
+
+def userinfo():
+    global user, userDetail
+    clear()
+    user = input('Qual user Quer puxar as informacoes? (email e linkedin)\n').casefold()
+    readUsers = open('userInfo.csv', 'r')
+    linhas = csv.reader(readUsers, delimiter=',')
+    userDetail = {}
+    for log in linhas:
+        userDetail[log[0]] = log[1:]
+    showUserInfo()
+
+def showUserInfo():
+    try:
+        clear()
+        info = print(f'User: {user} \nEmail: {userDetail[user][0]} \nLinkedin: {userDetail[user][1]}')
+        backToMenu = input(f'[0] Para retornar ao menu inicial\n ')
+        while not backToMenu == '0':
+            showUserInfo()
+        else:
+            mainMenu()
+    except KeyError:
+        noUserError = input(f'User {user} nao encontrado, Gostaria de Registra-lo?\n')
+        if noUserError.casefold() in ['sim','yes','y','s','quero']:
+            register()
+        else:
+            mainMenu()
+
+def loginfo():
+    userscsv = open('logInfo.csv', 'r')
+    linhas = csv.reader(userscsv, delimiter=',')
+    users = []
+    for log in linhas:
+        users.append(log)
+    for i in users:
+        print(i)
 
 mainMenu()
