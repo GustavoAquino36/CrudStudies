6a415bf89c9027dc3d91e4ab52b107ab205b5244
GustavoAquino36 <gustavoaquino357@gmail.com>
2022-11-20 13:36:20 -0300

    using csv for better usage of the data

--- Login.py
+++ Login.py
@@ -1,8 +1,29 @@
 from getpass import *
+import csv
 import os
 
-def mainMenu():
+def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
+
+def csvfile():
+    with open('C:/Users/Gustavo/Desktop/Estudo/crudEstudo/CrudStudies/logInfo.csv', 'r') as userscsv:
+        linhas = csv.reader(userscsv, delimiter = ',')
+        data = []
+        for log in linhas:
+            data.append(log)
+    for i in data:
+        print(i)
+    
+def userinfo():
+    with open('C:/Users/Gustavo/Desktop/Estudo/crudEstudo/CrudStudies/userInfo.csv', 'r') as usersinfo:
+        linhas = csv.reader(usersinfo, delimiter=',')
+        data = {}
+        for log in linhas:
+            data[log[0]] = log[1:]
+        print(data)
+
+def mainMenu():
+    clear()
     global escolha
     escolha = input('''
 ========== CRUD Menu ==========
@@ -17,13 +38,13 @@ def cursor():
     if escolha == '1':
         return registra()
 
-def loga():
+def loga(): # Log will be important later on
     user = input('Login: ')
     senha = getpass('Senha: ')
     logou = False
     data = open('txt data base path here', 'r')
     for log in data:
-        nome, password = log.split(' - ')
+        nome, password = log.split(',')
         password = password.strip()
         if nome == user and password == senha:
             logou = True
@@ -37,6 +58,9 @@ def registra():
         user = input('User a ser registrado: ')
         senha = getpass('Senha a ser registrada: ')
         data = open('txt data base path here', 'a')
-        data.write(f'\n{user} - {senha}')
+        data.write(f'\n{user},{senha}')
     finally:
         return mainMenu()
+
+userinfo() # learning about csv as the codes goes, still simple usage, improving...
+
